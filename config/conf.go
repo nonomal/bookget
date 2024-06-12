7131d27bc7ee1feb331c7152b15f5503a08015f8
zhudw <zhudwi@foxmail.com>
2024-06-12 19:27:08 +0800

    outlook邮箱

--- config/conf.go
+++ config/conf.go
@@ -206,7 +206,7 @@ func printHelp() {
 	printVersion()
 	fmt.Println(`Usage: bookget [OPTION]... [URL]...`)
 	flag.PrintDefaults()
-	fmt.Println("Email bug reports, questions, discussions to zhudw<zhudwi@foxmail.com>")
+	fmt.Println("Email bug reports, questions, discussions to zhudw<zhudwi@outlook.com>")
 	fmt.Println("and/or open issues at https://github.com/deweizhu/bookget/issues")
 }
 
